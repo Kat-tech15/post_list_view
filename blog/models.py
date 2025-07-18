e3c40f57cb64b5273862a883198f3eb4ee7e5b65
mikesplore <mikepremium8@gmail.com>
2025-07-18 16:57:16 +0300

    Revamped blog creation page

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
